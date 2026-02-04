61fdfe6fcdc173cd8af74edba0cf06a05099f23b
Tu Nombre <tu-email@example.com>
2026-02-04 11:31:06 -0500

    web

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let debounceTimer;
 let fuse = null;
 let fullData = [];
 
-const API_URL = 'https://bdf-production.up.railway.app/api/BDF/pdv';
+const API_URL = 'https://alqueria-production.up.railway.app/api/alqueria/pdv';
 
 // Cargar los datos de la API
 async function loadData() {
